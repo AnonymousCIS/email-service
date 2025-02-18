88d6b69fac4dc3ecf17fc060f83423d495998943
kks940104 <kks940104@gmail.com>
2025-02-18 16:39:11 +0900

    이메일

--- src/main/java/org/anonymous/EmailServiceApplication.java
+++ src/main/java/org/anonymous/EmailServiceApplication.java
@@ -1,11 +1,21 @@
 package org.anonymous;
 
+import com.netflix.discovery.EurekaClient;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class EmailServiceApplication {
+	@Autowired
+	private EurekaClient eurekaClient;
+
+	@PreDestroy
+	public void unregister() {
+		eurekaClient.shutdown();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(EmailServiceApplication.class, args);
 	}
